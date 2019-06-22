0d1e903053133ca18c12136aee704a2759478a00
nicolasGarciaMeli <alan.garcia@mercadolibre.com>
2019-06-22 17:05:30 -0300

    Added TODO tests

---
+++ src/test/java/com/revolut/unit/AccountTest.java
@@ -0,0 +1,21 @@
+package com.revolut.unit;
+
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+
+    @Test
+    void testAccountWithNullIdShouldThrownException(){
+        //TODO
+    }
+
+    @Test
+    void testAccountWithNullCurrencyShouldThrownException(){
+        //TODO
+    }
+
+    @Test
+    void testAccountWithNullBalanceShouldThrownException(){
+        //TODO
+    }
+}
---
+++ src/test/java/com/revolut/unit/TransactionTest.java
@@ -0,0 +1,4 @@
+package com.revolut.unit;
+
+public class TransactionTest {
+}
